3680ab82daafbf9f97e89d2c240bda0402ea1131
Doom-ux <KDmitriy@localhost.localdomain>
2020-03-12 17:47:28 +0700

    Add test.c

---
+++ test.c
@@ -0,0 +1,25 @@
+
+
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main()
+{   
+    int S,a,b;
+    printf("input a: ");
+    scanf("%d",%a);
+    printf("input b: ");
+    scanf("%d",%b);
+
+    S = a + b;
+
+    printf("Sum = %d",S);
+
+
+	return 0;
+}
+
